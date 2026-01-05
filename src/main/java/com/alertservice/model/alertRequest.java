3ccfed2baa6cb299d4c660ff7519eb198f0d3f8f
Levin <levinmathewshibu@gmail.com>
2026-01-05 13:11:25 +0400

    Complete dto files

--- src/main/java/com/alertservice/model/alertRequest.java
+++ src/main/java/com/alertservice/model/alertRequest.java
@@ -1,4 +1,21 @@
 package com.alertservice.model;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import java.time.Instant;
+import java.util.UUID;
+
+@Data
 public class alertRequest {
+    private UUID id;
+
+    @NotBlank(message = "Message Required")
+    private String message;
+
+    private Instant time;
+    private int severity;
+    private List<String> channels;
+    private List<String> recipients;
+    private Map<String,String> metadata;
+
 }
--- src/main/java/com/alertservice/model/alertResponse.java
+++ src/main/java/com/alertservice/model/alertResponse.java
@@ -1,4 +1,21 @@
 package com.alertservice.model;
 
+import lombok.Data;
+import java.util.List;
+import java.util.UUID;
+
+@Data
 public class alertResponse {
+    private UUID id;
+    private String message;
+    private String status;
+    private List<channelResult> channelResults;
+
+    @Data
+    public static class channelResult{
+        private String channel;
+        private boolean success;
+        private String message;
+        private Double cost;
+    }
 }
