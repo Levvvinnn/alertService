f75449f648c07b4e4acf538bc25e54265abdb7f0
Levin <levinmathewshibu@gmail.com>
2026-01-27 19:36:37 -0700

    alertResponse updated with Lombok annotations

--- src/main/java/com/alertservice/model/alertResponse.java
+++ src/main/java/com/alertservice/model/alertResponse.java
@@ -1,83 +1,31 @@
 package com.alertservice.model;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
 import java.util.List;
 import java.util.UUID;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class alertResponse {
     private UUID id;
     private String message;
     private String status;
     private List<channelResult> channelResults;
 
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public List<channelResult> getChannelResults() {
-        return channelResults;
-    }
-
-    public void setChannelResults(List<channelResult> channelResults) {
-        this.channelResults = channelResults;
-    }
-
-    public static class channelResult{
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class channelResult {
         private String channel;
         private boolean success;
         private String message;
         private Double cost;
-
-        public boolean isSuccess() {
-            return success;
-        }
-
-        public void setSuccess(boolean success) {
-            this.success = success;
-        }
-
-        public String getMessage() {
-            return message;
-        }
-
-        public void setMessage(String message) {
-            this.message = message;
-        }
-
-        public Double getCost() {
-            return cost;
-        }
-
-        public void setCost(Double cost) {
-            this.cost = cost;
-        }
-
-        public String getChannel() {
-            return channel;
-        }
-
-        public void setChannel(String channel) {
-            this.channel = channel;
-        }
     }
-
 }
