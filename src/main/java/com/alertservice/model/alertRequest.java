367a6b617012c28ce1dc795894344be96b93b447
Levin <levinmathewshibu@gmail.com>
2026-01-27 19:24:41 -0700

    lombok version update and alertRequest class modification

--- src/main/java/com/alertservice/model/alertRequest.java
+++ src/main/java/com/alertservice/model/alertRequest.java
@@ -3,11 +3,15 @@ package com.alertservice.model;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 import java.util.List;
 import java.util.Map;
 
 @Data // Generates getters, setters, equals, hashCode, and toString methods
+@NoArgsConstructor // Generates a no-argument constructor
+@AllArgsConstructor // Generates an all-argument constructor
 @Builder // Adds a builder pattern for object creation
 public class alertRequest {
 
